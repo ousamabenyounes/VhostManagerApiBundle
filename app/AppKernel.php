fb53208da69af9203453d36e1c66f398e4586a79
Ousama Ben Younes <contact@lesmuslims.fr>
2013-10-31 00:06:08 +0100

    Added JMSSerializer + FOSRestBundle on AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new VhostManager\ApiBundle\VhostManagerApiBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
