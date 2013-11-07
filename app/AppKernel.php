8a8249be2f771ad04a70f808bd425a5e8d3c2653
Ousama Ben Younes <contact@lesmuslims.fr>
2013-11-07 01:48:22 +0100

    Added getUser + getUsers controller logic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new VhostManager\ApiBundle\VhostManagerApiBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
