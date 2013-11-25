b21692afa1aae82ef39a4f970425d823020da1bd
Ousama Ben Younes <contact@lesmuslims.fr>
2013-11-26 00:28:01 +0100

    Remove acmeBundle + added users route formating

--- src/VhostManager/ApiBundle/Controller/UsersController.php
+++ src/VhostManager/ApiBundle/Controller/UsersController.php
@@ -13,8 +13,10 @@ use FOS\RestBundle\Controller\Annotations\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use VhostManager\ApiBundle\Entity\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use VhostManager\ApiBundle\Controller\WebServiceController;
 
-class UsersController extends Controller
+
+class UsersController extends WebServiceController
 {
 
     /**
@@ -26,29 +28,13 @@ class UsersController extends Controller
 
         $users = $this->getDoctrine()->getRepository('VhostManagerApiBundle:User')
             ->findAll();
-        var_dump($users);
+    //    var_dump($users);
 
-        return new JsonResponse(array('name' => 'ous'));
-
-
-
-        return array('users' => array('1' => 22));
-
-
-
-        return array('users' => $users);
+        $users = $this->formatUsers($users);
+        return new JsonResponse(array('users' => $users));
     }
 
 
-    /**
-     * @param User $user
-     * @return array
-     * @View()
-     * @ParamConverter("user", class="VhostManagerApiBundle:User")
-     */
-    public function getUserAction(User $user)
-    {
-        return array('user' => $user);
-    }
+
 
 } 
--- src/VhostManager/ApiBundle/Controller/WebServiceController.php
+++ src/VhostManager/ApiBundle/Controller/WebServiceController.php
@@ -6,14 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use VhostManager\ApiBundle\Entity\User;
+
+
 class WebServiceController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
+
+    public function formatUsers($users) {
+        $output = array();
+        foreach ($users as $user) {
+            $output[] = array(
+                'id' => $user->getId(),
+                'name' => $user->getUserName()
+            );
+        }
+       return $output;
     }
+
 }
